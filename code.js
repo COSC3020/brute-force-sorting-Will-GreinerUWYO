0cf8d0dc70b8677fd0d2a0796019f801af1a833f
wgreine2 <144729039+Will-GreinerUWYO@users.noreply.github.com>
2024-11-11 00:19:21 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,37 @@
 function permutationSort(a) {
-    return 0;
+    return nextPermutation(a.length, a, 0);
+}
+
+function nextPermutation(iteration, a, permutations) {
+    if (!checkSort(a)) {
+        if (iteration === 1) {
+            return permutations;
+        } else {
+            permutations++;
+            permutations = nextPermutation(iteration - 1, a, permutations);
+            var temp = 0;
+
+            for (var i = 0; i < iteration - 1; i++) {
+                if (iteration % 2 === 0) {
+                    temp = a[i];
+                    a[i] = a[iteration - 1];
+                    a[iteration - 1] = temp;
+                } else {
+                    temp = a[0];
+                    a[0] = a[iteration - 1];
+                    a[iteration - 1] = temp;
+                }
+            }
+        }
+    }
+    return permutations;
+}
+
+function checkSort(a) {
+    for (var i = 0; i < a.length - 1; i++) {
+        if (a[i] > a[i + 1]) {
+            return false;
+        }
+    }
+    return true;
 }
