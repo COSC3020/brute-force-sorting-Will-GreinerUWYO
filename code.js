1e5f545c7ec1a292e327a45046b3f8953b476f25
wgreine2 <144729039+Will-GreinerUWYO@users.noreply.github.com>
2024-11-12 00:46:39 -0700

    Update code.js

--- code.js
+++ code.js
@@ -3,24 +3,30 @@ function permutationSort(a) {
 }
 
 function nextPermutation(iteration, a, permutations) {
-    if (!checkSort(a)) {
-        if (iteration === 1) {
-            return permutations;
-        } else {
-            permutations++;
-            permutations = nextPermutation(iteration - 1, a, permutations);
-            var temp = 0;
+    if (checkSort(a)) {
+        return permutations;
+    }
+    if (iteration === 1) {
+        return permutations;
+    } 
+    else {
+        permutations++;
 
-            for (var i = 0; i < iteration - 1; i++) {
-                if (iteration % 2 === 0) {
-                    temp = a[i];
-                    a[i] = a[iteration - 1];
-                    a[iteration - 1] = temp;
-                } else {
-                    temp = a[0];
-                    a[0] = a[iteration - 1];
-                    a[iteration - 1] = temp;
-                }
+        for (var i = 0; i < iteration - 1; i++) {
+            permutations = nextPermutation(iteration - 1, a, permutations)
+
+            if (checkSort(a)) {
+                return permutations;
+            }
+
+            if (iteration % 2 === 0) {
+                let temp = a[i];
+                a[i] = a[iteration - 1];
+                a[iteration - 1] = temp;
+            } else {
+                let temp = a[0];
+                a[0] = a[iteration - 1];
+                a[iteration - 1] = temp;
             }
         }
     }
