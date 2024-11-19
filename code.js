8cc11419939d84cc9b95d7514b3ed1fe6d1792ec
wgreine2 <144729039+Will-GreinerUWYO@users.noreply.github.com>
2024-11-18 19:49:51 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,31 +1,40 @@
 function permutationSort(a) {
-    return nextPermutation(a.length, a, 0);
+    let length = a.length;
+    let c = [];
+    let temp = 0;
+    let permutations = 0;
+    for(let i = 0; i < length; i++) {
+        c[i] = 0;
+    }
+    
+    let i = 1;
+    while(i < length) {
+        if(checkSort(a)) {
+            return permutations;
+        }
+        if (c[i] < i) {
+            if (i%2 === 0) {
+                temp = a[0];
+                a[0] = a[i];
+                a[i] = temp;
+            }
+            else{
+                temp = a[c[i]];
+                a[c[i]] = a[i];
+                a[i] = temp;
+            }
+            c[i] += 1;
+            i = 1;
+            permutations++;
+        }
+        else {
+            c[i] = 0;
+            i += 1;
+        }
+    }
+    return permutations;
 }
 
-function nextPermutation(iteration, a, permutations) {
-    if (checkSort(a)) {
-        return permutations;
-    }
-    else {
-        permutations++;
-        permutations = nextPermutation(iteration - 1, a, permutations);
-        for (var i = 0; i < iteration - 1; i++) {
-            if (checkSort(a)) {
-                return permutations;
-            }
-            if (iteration % 2 === 0) {
-                let temp = a[i];
-                a[i] = a[iteration - 1];
-                a[iteration - 1] = temp;
-            } else {
-                let temp = a[0];
-                a[0] = a[iteration - 1];
-                a[iteration - 1] = temp;
-            }
-        }
-        return nextPermutation(iteration - 1, a, permutations);
-    }
-}
 
 function checkSort(a) {
     for (var i = 0; i < a.length - 1; i++) {
