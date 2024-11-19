1981a73391defea6d259e677be87c13c02f67b5f
wgreine2 <144729039+Will-GreinerUWYO@users.noreply.github.com>
2024-11-18 20:08:08 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,33 +1,44 @@
 function permutationSort(a) {
     let length = a.length;
-    let c = [];
+    let c = []; // control array
     let temp = 0;
     let permutations = 0;
+
+    // builds the control array
+    // the control array works by incrementing c[i] whenever a swap is performed on the [i] element
+    // when this array is reset, c[i] = i, and then all swaps completed. For example, no swaps at i = 0, 1 swap at i = 1 etc.
+    // c[i] is reset following a of i, since there is now a new option at c[i] and all previous permutations are done.
     for(let i = 0; i < length; i++) {
         c[i] = 0;
     }
     
     let i = 1;
+    // uses an iterative version of Heap's algorithm.
     while(i < length) {
+        // if the array is sorted, returns permutations.
         if(checkSort(a)) {
             return permutations;
         }
         if (c[i] < i) {
+            // if the index is even, swap the first value with the value at the index.
             if (i%2 === 0) {
                 temp = a[0];
                 a[0] = a[i];
                 a[i] = temp;
             }
+            // if the index is odd, swap the value at the index with the value at the control array's index.
             else{
                 temp = a[c[i]];
                 a[c[i]] = a[i];
                 a[i] = temp;
             }
+            // Resets index and increments control and permutations.
             c[i] += 1;
             i = 1;
             permutations++;
         }
         else {
+            // resets control array, increments index
             c[i] = 0;
             i += 1;
         }
@@ -35,7 +46,7 @@ function permutationSort(a) {
     return permutations;
 }
 
-
+// simple helper function to ensure is sorted.
 function checkSort(a) {
     for (var i = 0; i < a.length - 1; i++) {
         if (a[i] > a[i + 1]) {
